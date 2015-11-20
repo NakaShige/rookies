f779970eab61f49a79f9a7074e7b00dcf1fa56fe
sh-nakagawara <shigekibox@gmail.com>
2015-11-20 23:18:04 +0900

    一辺の長さの入力チェックを追加（正の実数のみ許可するように変更）

--- 面積管理システム/面積管理システム.View/AddFigureDialog.cs
+++ 面積管理システム/面積管理システム.View/AddFigureDialog.cs
@@ -23,26 +23,40 @@ namespace 面積管理システム.View
         {
             get
             {
-                double dLength;
-                if (!double.TryParse(txtLength.Text, out dLength))
+                double? length = this.Length;
+                if (!length.HasValue)
                     return null;
 
-                if (rbnSquare.Checked)
-                {
-                    return new 四角形(dLength);
-                }
-                else if (rbnTriangle.Checked)
-                {
-                    return new 三角形(dLength);
-                }
+                if (rbnSquare.Checked) return new 四角形(length.Value);
+                else if (rbnTriangle.Checked) return new 三角形(length.Value);
+                
                 return null;
             }
         }
 
         private void btnOK_Click(object sender, EventArgs e)
         {
+            double? length = this.Length;
+            if (!length.HasValue)
+            {
+                MessageBox.Show("長さの入力が間違っています。");
+                return;
+            }
+
             this.DialogResult = DialogResult.OK;
             this.Close();
+        }
+
+        private double? Length
+        {
+            get
+            {
+                double dLength;
+                if (!double.TryParse(txtLength.Text, out dLength) || dLength <= 0.0)
+                    return null;
+
+                return dLength;
+            }
         }
     }
 }
